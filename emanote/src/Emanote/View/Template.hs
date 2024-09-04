98816eadd2a756e5de664f3b0a5dc38dd30ff56e
Drewry Pope <drewrypope@gmail.com>
2024-09-04 01:37:37 -0500

    negate

--- emanote/src/Emanote/View/Template.hs
+++ emanote/src/Emanote/View/Template.hs
@@ -291,8 +291,8 @@ routeTreeSplices tCtx mCurrentRoute model = do
       let tr = last path
           isLeaf = null children
           priority = if getFoldersFirst tr && isLeaf then 1 else 0 :: Int
-       in ( priority,
-            Meta.lookupRouteMeta @Int 0 (one "order") tr model,
+       in ( negate priority,
+            negate $ Meta.lookupRouteMeta @Int 0 (one "order") tr model,
             tr
           )
     getCollapsed tr =
